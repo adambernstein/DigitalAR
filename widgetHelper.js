0af1bd22400a0e674dfe209488d241ab3a753de3
Adam Bernstein <abernstein@pih.org>
2016-01-22 16:25:55 -0500

    some more minor tweaks. next to link up photos to stories. Pictures loading weirdly in FF but not other browsers

--- widgetHelper.js
+++ widgetHelper.js
@@ -35,24 +35,24 @@ function switchColors(c) {
 			break;
 		case "a-housecalls":
 			mc.style.backgroundColor = "#008ea0";
-			ca[0].style.opacity="0";
 			ca[1].style.opacity="1";
+			ca[0].style.opacity="0";			
 			ca[2].style.opacity="0";
 			ca[3].style.opacity="0";
 			break;
 		case "a-healthsystems":
 			mc.style.backgroundColor = "#f6be00";
-			ca[0].style.opacity="0";
-			ca[1].style.opacity="0";
 			ca[2].style.opacity="1";
+			ca[0].style.opacity="0";
+			ca[1].style.opacity="0";			
 			ca[3].style.opacity="0";
 			break;
 		case "a-westay":
 			mc.style.backgroundColor = "#323372";
+			ca[3].style.opacity="1";
 			ca[0].style.opacity="0";
 			ca[1].style.opacity="0";
-			ca[2].style.opacity="0";
-			ca[3].style.opacity="1";
+			ca[2].style.opacity="0";			
 			break;
 	}
 }
