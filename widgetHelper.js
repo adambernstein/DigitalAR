c5cdebe8f803dde00c9cae49363b0ad8dc7bd053
Adam Bernstein <abernstein@pih.org>
2016-01-22 12:02:08 -0500

    needed to separate js and reference there. hosted on codepen for now but will have to change to BSD tools.

---
+++ widgetHelper.js
@@ -0,0 +1,41 @@
+function change(e) {
+	
+	var mlinks = document.getElementsByClassName("mlink");
+	var mbg = document.getElementById('menuContainer');
+	for (i = 0; i < mlinks.length; i++) {
+		//unset all other font sizes
+		if (mlinks[i].innerHTML != e.innerHTML) {
+			mlinks[i].style.fontSize = "2em";
+			mlinks[i].style.opacity = ".5";
+			//mlinks[i].style.lineHeight="2em";
+		} else {
+			//set current link style
+			e.style.fontSize = "3.3em";
+			//e.style.lineHeight="1em";
+			e.style.opacity = "1";
+			switchColors(e);
+
+		}
+	}
+
+}
+
+function switchColors(c) {
+	var mc = document.getElementById('menuContainer');
+	c.style.color = "#fff";
+	switch (c.id) {
+		case "a-wego":
+			mc.style.backgroundColor = "#F8971d";
+			break;
+		case "a-housecalls":
+			mc.style.backgroundColor = "#008ea0";
+			break;
+		case "a-healthsystems":
+			mc.style.backgroundColor = "#f6be00";
+			break;
+		case "a-westay":
+			mc.style.backgroundColor = "#323372";
+			break;
+	}
+
+}
