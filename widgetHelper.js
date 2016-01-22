21af7e9175ce13fe344495aa498a3bfb0c8bbbab
Adam Bernstein <abernstein@pih.org>
2016-01-22 14:19:15 -0500

    looks good in codepen, but may need to re-do image transition process

--- widgetHelper.js
+++ widgetHelper.js
@@ -1,7 +1,8 @@
 function change(e) {
-	
+
 	var mlinks = document.getElementsByClassName("mlink");
 	var mbg = document.getElementById('menuContainer');
+
 	for (i = 0; i < mlinks.length; i++) {
 		//unset all other font sizes
 		if (mlinks[i].innerHTML != e.innerHTML) {
@@ -22,20 +23,36 @@ function change(e) {
 
 function switchColors(c) {
 	var mc = document.getElementById('menuContainer');
+	var ca = document.getElementById('contentContainer').children;
 	c.style.color = "#fff";
 	switch (c.id) {
 		case "a-wego":
 			mc.style.backgroundColor = "#F8971d";
+			ca[0].style.opacity="1";
+			ca[1].style.opacity="0";
+			ca[2].style.opacity="0";
+			ca[3].style.opacity="0";
 			break;
 		case "a-housecalls":
 			mc.style.backgroundColor = "#008ea0";
+			ca[0].style.opacity="0";
+			ca[1].style.opacity="1";
+			ca[2].style.opacity="0";
+			ca[3].style.opacity="0";
 			break;
 		case "a-healthsystems":
 			mc.style.backgroundColor = "#f6be00";
+			ca[0].style.opacity="0";
+			ca[1].style.opacity="0";
+			ca[2].style.opacity="1";
+			ca[3].style.opacity="0";
 			break;
 		case "a-westay":
 			mc.style.backgroundColor = "#323372";
+			ca[0].style.opacity="0";
+			ca[1].style.opacity="0";
+			ca[2].style.opacity="0";
+			ca[3].style.opacity="1";
 			break;
 	}
-
 }
