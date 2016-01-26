b9b93984b6cf027d32cf2bf480be728a3b69ba95
Adam Bernstein <abernstein@pih.org>
2016-01-26 16:53:08 -0500

    JS code re-write, still need minor fixes to default state and menuContainer font size. Mobile next.

--- widgetHelper.js
+++ widgetHelper.js
@@ -6,7 +6,7 @@ function change(e) {
 	for (i = 0; i < mlinks.length; i++) {
 		//unset all other font sizes
 		if (mlinks[i].innerHTML != e.innerHTML) {
-			mlinks[i].style.fontSize = "2em";
+			mlinks[i].style.fontSize = "2.2em";
 			mlinks[i].style.opacity = ".5";
 			//mlinks[i].style.lineHeight="2em";
 		} else {
@@ -24,11 +24,55 @@ function change(e) {
 function switchColors(c) {
 	var mc = document.getElementById('menuContainer');
 	var ca = document.getElementById('contentContainer').children;
-	c.style.color = "#fff";
+	var currentIndex = (function(){
+		switch (c.id) {
+			case "a-wego":	
+				return 0;
+				break;
+			case "a-housecalls":
+				return 1;
+				break;
+			case "a-healthsystems":
+				return 2;
+				break;
+			case "a-westay":
+				return 3;
+				break;
+		}
+	})();
+	
+	
+	for (y=0;y<ca.length;y++){
+		if (y!=currentIndex){
+			ca[y].style.opacity="0";
+			ca[y].style.zIndex="0";
+		}
+		else {
+			ca[y].style.zIndex="4";
+			ca[y].style.opacity="1";
+		}
+		switch (currentIndex){
+			case 0:
+				mc.style.backgroundColor = "#F8971d";
+				break;
+			case 1:
+				mc.style.backgroundColor = "#008ea0";
+				break;
+			case 2:
+				mc.style.backgroundColor = "#f6be00";
+				break;
+			case 3:
+				mc.style.backgroundColor = "#323372";
+				break;
+		}
+	}
+	
+	/* old **
 	switch (c.id) {
 		case "a-wego":
 			mc.style.backgroundColor = "#F8971d";
 			ca[0].style.opacity="1";
+			ca[0].style.zIndex="4";
 			ca[1].style.opacity="0";
 			ca[2].style.opacity="0";
 			ca[3].style.opacity="0";
@@ -54,5 +98,5 @@ function switchColors(c) {
 			ca[1].style.opacity="0";
 			ca[2].style.opacity="0";			
 			break;
-	}
+	}*/
 }
